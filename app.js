b8f98f1654bfb723852a277d8c40f7c961f98f55
KurtReti <kurt.reti@gmail.com>
2022-09-08 10:49:16 +1000

    adding navigation and fixing css for mobile

---
+++ app.js
@@ -0,0 +1,19 @@
+function hideProjects() {
+    var x = document.getElementById("project-container");
+    if (x.classList.contains("hide")) {
+        x.classList.remove("hide");
+      } else {
+        x.classList.add("hide");
+      }
+
+    var y = document.getElementById("container");
+    var z = document.getElementById("divider-line");
+    var a = document.getElementById("divider-title")
+    
+    // a.textContent = "WHO ARE WE";
+    y.style.backgroundColor = "black";
+    // y.style.color = "white";
+    y.style.animation = "mymove 5s 1";
+    // z.style.backgroundColor = "white";
+
+};
